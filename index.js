6b00e2a1998483d00e3b98f5cc204dd9e443f5f1
Thiago Mota <th.araujomota@gmail.com>
2022-08-02 20:18:54 -0300

    feat: adds GET /talker/:id endpoint

--- index.js
+++ index.js
@@ -6,13 +6,35 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
+const TALKER_NOT_FOUND_MESSAGE = { message: 'Pessoa palestrante não encontrada' };
 const PORT = '3000';
 
-app.get('/talker', async (_request, response) => {
-  const data = await fs.readFile('talker.json', 'utf8');
-  return response
+const readTalkerJSON = async () => {
+  const talkers = await fs.readFile('talker.json', 'utf8');
+  return JSON.parse(talkers);
+};
+
+app.get('/talker', async (_request, response) => (
+  response
     .status(HTTP_OK_STATUS)
-    .send(JSON.parse(data));
+    .send(await readTalkerJSON())
+));
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await readTalkerJSON();
+
+  const specificTalker = talkers.find((talker) => talker.id === Number(id));
+
+  if (!specificTalker) {
+    return response
+      .status(HTTP_NOT_FOUND_STATUS)
+      .json(TALKER_NOT_FOUND_MESSAGE);
+  }
+
+  return response
+  .send(specificTalker);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
