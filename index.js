30ac22f6cbb6d096264960cf2b15c0ad0c719f64
Thiago Mota <th.araujomota@gmail.com>
2022-08-02 19:11:13 -0300

    feat: adds GET /talker endpoint

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (_request, response) => {
+  const data = await fs.readFile('talker.json', 'utf8');
+  return response
+    .status(HTTP_OK_STATUS)
+    .send(JSON.parse(data));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
