d498ddc0162b22d8e9f2818e079a21d90229f217
Thiago Mota <th.araujomota@gmail.com>
2022-08-05 07:41:28 -0300

    refac: fix wrong errorMessages

--- helpers/messages/errorMessages.js
+++ helpers/messages/errorMessages.js
@@ -7,7 +7,7 @@
     AGE_REQUIRED: 'O campo "age" é obrigatório',
     INVALID_AGE: 'A pessoa palestrante deve ser maior de idade',
     TALK_REQUIRED: 'O campo "talk" é obrigatório',
-    WATCHED_AT_REQUIRED: 'O campo "watchedAt é obrigatório',
+    WATCHED_AT_REQUIRED: 'O campo "watchedAt" é obrigatório',
     INVALID_DATE: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     RATE_REQUIRED: 'O campo "rate" é obrigatório',
     INVALID_RATE: 'O campo "rate" deve ser um inteiro de 1 à 5',
